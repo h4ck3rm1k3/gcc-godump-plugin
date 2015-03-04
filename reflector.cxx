6a1297adb0c14f354eb26af4ca8de78caabe71af
James Michael DuPont <JamesMikeDuPont@gmail.com>
2015-03-03 23:28:19 -0600

    reflector

---
+++ reflector.cxx
@@ -0,0 +1,56 @@
+
+
+class some_type {};
+
+class field_type {
+  some_type parent_class;
+};
+
+class field_decl {
+  field_type type;  
+  identifier name;
+};
+
+class field_list {
+  void add(field_decl item);
+};
+
+class class_decl {
+  field_list fields;
+};
+
+class param_type {
+  some_type type;
+};
+
+class identifier {
+};
+
+class param_decl {
+  param_type type;  
+  identifier name;
+};
+
+class param_list {
+  add(param_decl &);
+};
+
+class method {
+  param_list params;
+};
+
+
+
+class lang_output {
+  void emit(class_decl &);
+  void emit(field_decl &);
+  void emit(field_type &);
+  void emit(method     &);
+  void emit(param_decl &);
+  void emit(param_type &);
+};
+
+
+class golang_output {
+  lang_output parent_class;
+};
